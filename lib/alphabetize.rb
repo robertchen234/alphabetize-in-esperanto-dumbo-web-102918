fbb2cd2602f92b2d2a8f01edec7b67d11a2e15e2
Robert Chen <robertchen234@gmail.com>
2018-10-25 18:43:19 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,11 +1,6 @@
-esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-
 def alphabetize(arr)
-
-  arr.sort_by do |phrase|
-    phrase.chars.collect do |letter|
-      esperanto.index(letter)
-    end
-  end
   
+  special = { 'ĉ' => 'c', 'ĝ' => 'g', 'ĥ' => 'h', 'ĵ' => 'j', 'ŝ' => 's', 'ŭ' => 'u' }
+  
+  arr.each do |
 end
