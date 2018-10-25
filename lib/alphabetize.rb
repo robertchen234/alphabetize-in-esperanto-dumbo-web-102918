cb604d0ae10190cc77ea4286365480d12d6f1666
Robert Chen <robertchen234@gmail.com>
2018-10-25 17:47:23 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,13 +3,16 @@ def alphabetize(arr)
   new_arr = []
   
   arr.each do |word|
-    "ĉ" = "c" if word.include?("ĉ")
-    "ĝ" = "g" if word.include?("ĝ")
-    "ĥ" = "h" if word.include?("ĥ")
-    "ĵ" = "j" if word.include?("ĵ")
-    "ŝ" = "s" if word.include?("ŝ")
-    "ŭ" = "u" if word.include?("ŭ")
-    new_arr << word
+    i = 0 
+    while i < word.length
+      "ĉ" = "c" if word.include?("ĉ")
+      "ĝ" = "g" if word.include?("ĝ")
+      "ĥ" = "h" if word.include?("ĥ")
+      "ĵ" = "j" if word.include?("ĵ")
+      "ŝ" = "s" if word.include?("ŝ")
+      "ŭ" = "u" if word.include?("ŭ")
+      new_arr << word
+      i += 1
   end
   
   new_arr
