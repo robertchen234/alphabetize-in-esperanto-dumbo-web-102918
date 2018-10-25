244524183a4c29d140c61e6e8f4e115fec92a37d
Robert Chen <robertchen234@gmail.com>
2018-10-25 17:51:30 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,12 +5,12 @@ def alphabetize(arr)
   arr.each do |phrase|
     i = 0 
     while i < phrase.length
-      "ĉ" = "c" if phrase[i] == "ĉ"
-      "ĝ" = "g" if phrase[i] == "ĝ"
-      "ĥ" = "h" if phrase[i] == "ĥ"
-      "ĵ" = "j" if phrase[i] == "ĵ"
-      "ŝ" = "s" if phrase[i] == "ŝ"
-      "ŭ" = "u" if phrase[i] == "ŭ"
+      ĉ = c if phrase[i] == "ĉ"
+      ĝ = g if phrase[i] == "ĝ"
+      ĥ = h if phrase[i] == "ĥ"
+      ĵ = j if phrase[i] == "ĵ"
+      ŝ = s if phrase[i] == "ŝ"
+      ŭ = u if phrase[i] == "ŭ"
       new_arr << phrase
       i += 1
     end
