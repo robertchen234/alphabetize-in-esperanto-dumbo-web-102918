148006fc9a6a4b113b0e926c491422543f9fe29c
Robert Chen <robertchen234@gmail.com>
2018-10-25 18:50:18 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,11 @@
 ESPERANTO = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
+  
   arr.sort_by do |word| 
     word.split('').collect do |letter|
       ESPERANTO.index(letter)
     end
   end
+  
 end
