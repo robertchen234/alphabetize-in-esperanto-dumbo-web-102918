35bda422c8eb0d194414919f0724a525ba689642
Robert Chen <robertchen234@gmail.com>
2018-10-25 18:15:51 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,9 @@ def alphabetize(arr)
   new_arr = []
   
   arr.each do |phrase|
+    
     i = 0 
+
     while i < phrase.length
       phrase[i] = "c" if phrase[i] == "ĉ"
       phrase[i] = "g" if phrase[i] == "ĝ"
@@ -13,24 +15,25 @@ def alphabetize(arr)
       phrase[i] = "u" if phrase[i] == "ŭ"
       i += 1
     end
+    
     new_arr << phrase
     
-    
-    
-    
-  # new_arr.collect do |words|
-  #   j = 0
-  #   while j < words.length
-  #     words[j] = "ĉ" if words[j] == "c"
-  #     words[j] = "ĝ" if words[j] == "g"
-  #     words[j] = "ĥ" if words[j] == "h"
-  #     words[j] = "ĵ" if words[j] == "j"
-  #     words[j] = "ŝ" if words[j] == "s"
-  #     words[j] = "ŭ" if words[j] == "u"
-  #     j += 1
-  #   end
-
-  # end
   end
+  
   new_arr.sort
+  
+    new_arr.collect do |words|
+    j = 0
+    while j < words.length
+      words[j] = "ĉ" if words[j] == "c"
+      words[j] = "ĝ" if words[j] == "g"
+      words[j] = "ĥ" if words[j] == "h"
+      words[j] = "ĵ" if words[j] == "j"
+      words[j] = "ŝ" if words[j] == "s"
+      words[j] = "ŭ" if words[j] == "u"
+      j += 1
+    end
+
+  end
+  
 end
