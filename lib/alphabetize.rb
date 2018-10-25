6525fe1fe6efa5c437b3c8b9d9f5399be75f7ef5
Robert Chen <robertchen234@gmail.com>
2018-10-25 18:38:30 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,20 +1,6 @@
 def alphabetize(arr)
   
-  special = {
+  special = { 'ĉ' => 'c', 'ĝ' => 'g', 'ĥ' => 'h', 'ĵ' => 'j', 'ŝ' => 's', 'ŭ' => 'u' }
   
-  original = ['ĉ','ĝ','ĥ','ĵ','ŝ','ŭ']
-  replacement = ['c_','g_','h_','j_','s_','u_']
-  
-  arr.each do |str|
-    str.gsub!(original, replacement)
-  end
-  
-  arr.sort!
-  
-  arr.each do |str|
-    str.gsub!(replacement, original)
-  end
-  
-  return arr
-  
+  arr.each do |
 end
