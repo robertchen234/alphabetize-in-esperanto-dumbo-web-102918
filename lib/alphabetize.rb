ed504c0559aac15fa0c28a8a491400ee300cad63
Robert Chen <robertchen234@gmail.com>
2018-10-25 17:46:25 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,12 +3,12 @@ def alphabetize(arr)
   new_arr = []
   
   arr.each do |word|
-    ĉ = c if word.include?("ĉ")
-    ĝ = g if word.include?("ĝ")
-    ĥ = h if word.include?("ĥ")
-    ĵ = j if word.include?("ĵ")
-    ŝ = s if word.include?("ŝ")
-    ŭ = u if word.include?("ŭ")
+    "ĉ" = "c" if word.include?("ĉ")
+    "ĝ" = "g" if word.include?("ĝ")
+    "ĥ" = "h" if word.include?("ĥ")
+    "ĵ" = "j" if word.include?("ĵ")
+    "ŝ" = "s" if word.include?("ŝ")
+    "ŭ" = "u" if word.include?("ŭ")
     new_arr << word
   end
   
