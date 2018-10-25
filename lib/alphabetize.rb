be288fb93803f7cfca2febe0743db465f583f686
Robert Chen <robertchen234@gmail.com>
2018-10-25 18:32:37 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,15 @@
 def alphabetize(arr)
   
-  new_arr = []
+  arr.each do |str|
+    str.gsub!(['ĉ','ĝ','ĥ','ĵ','ŝ','ŭ'], ['c_','g_','h_','j_','s_','u_'])
+  end
+  
+  arr.sort!
   
   arr.each do |str|
-    str.gsub!(['ĉ','ĝ','ĥ','ĵ','ŝ'], ['c_'])
+    str.gsub!( ['c_','g_','h_','j_','s_','u_'], ['ĉ','ĝ','ĥ','ĵ','ŝ','ŭ'])
   end
+  
+  return arr
   
 end
