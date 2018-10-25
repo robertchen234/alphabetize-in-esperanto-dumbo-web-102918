68509b720d2315738d9a1aa28538c84023e3880d
Robert Chen <robertchen234@gmail.com>
2018-10-25 18:49:29 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ ESPERANTO = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   arr.sort_by do |word| 
     word.split('').collect do |letter|
-      ESPERANTO .index(letter)
+      ESPERANTO.index(letter)
     end
   end
 end
